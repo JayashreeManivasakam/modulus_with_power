b81b67a2cee2bd120efba49fb0c9a34b10f78ab7
JayashreeManivasakam <jaymanivasakam.gmail.com>
2023-07-18 23:01:03 +0530

    INITIAL COMMIT HAS BEEN DONE

---
+++ src/Power_with_modulus.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Power_with_modulus {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value for a,b,c");
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        int c= sc.nextInt();
+        int result=1;
+        for (int i=0;i<b;i++){
+            result*=a;
+        }
+        System.out.println(result%c);
+    }
+}
